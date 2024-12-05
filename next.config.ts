955733c1cd799860b4391710c7569bccc067c8c1
AtitCh <atit8794@gmail.com>
2024-12-06 06:30:33 +0700

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export', // <=== enables static exports
+  output: 'export', // Enables static export
+  basePath: '/resume', // Replace <repository-name> with your GitHub repository name
+  assetPrefix: '/resume', // Same as basePath
   reactStrictMode: true,
 };
 
